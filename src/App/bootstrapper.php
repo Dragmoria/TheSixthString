0897039c655362969316e8a2f198ffad8bd5cc82
Jarne Kompier <j.kompier@hotmail.nl>
2023-12-01 00:11:46 +0100

    Revert "Demo"
    This reverts commit bbd7ba4193f89bf5f1941420c7b1a51979e41ea0.

--- src/App/bootstrapper.php
+++ src/App/bootstrapper.php
@@ -1,12 +1,9 @@
 <?php
 
-use Http\Controllers\HomeController;
 use Lib\MVCCore\Containers\Container;
 use Lib\MVCCore\Application;
 use Lib\MVCCore\Routers\HTTPStatusCodes;
 use Lib\EnvUtility\EnvHandler;
-use Models\DailyMessageModel;
-use Models\ProductsModel;
 
 Application::initialize();
 
@@ -15,15 +12,11 @@ $container = Container::getInstance();
 $container->registerClass(EnvHandler::class)->asSingleton()->setResolver(function() {
     return new EnvHandler(BASE_PATH . '/.env');
 });
-$container->registerClass(DailyMessageModel::class)->asSingleton();
-$container->registerClass(ProductsModel::class)->asSingleton();
 
 $router = Application::getRouter();
 $router->registerStatusView(HTTPStatusCodes::NOT_FOUND, VIEWS_PATH . '/Errors/404.php');
 // Add routes below here.
 
-$router->get('/', [HomeController::class, 'index']);
-$router->put('/', [HomeController::class, 'put']);
 
 // Run the application.
 Application::run();
