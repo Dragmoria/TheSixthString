5a2bf4789d1e826f4b262de8b50a3032eed20bae
Jarne Kompier <j.kompier@hotmail.nl>
2023-12-09 17:55:00 +0100

    Changed Role enum
    Added two new roles and a method to check if one role has the rights of another

--- src/Lib/Enums/Role.php
+++ src/Lib/Enums/Role.php
@@ -4,5 +4,11 @@ namespace Lib\Enums;
 
 enum Role: int {
     case Customer = 0;
-    case Admin = 1;
+    case Analyst = 1;
+    case Manager = 2;
+    case Admin = 3;
+
+    public function hasRightsOf(Role $role): bool {
+        return $this->value >= $role->value;
+    }
 }
