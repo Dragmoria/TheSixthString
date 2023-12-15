6f2eed252cfdc398798a20efe05191d738bc3cce
Jerdi Pol <jerdipol-admin@deltion.nl>
2023-12-15 11:48:57 +0100

    Front-end update

---
+++ src/Http/Controllers/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Http\Controllers;
+
+use Lib\MVCCore\Controller;
+use Lib\MVCCore\Routers\Responses\Response;
+use Lib\MVCCore\Routers\Responses\ViewResponse;
+
+class IndexController extends Controller {
+    public function show(): ?Response {
+        $response = new ViewResponse();
+        $response->setBody(view(VIEWS_PATH . 'Index.view.php', [
+            'Hallo'=>'Doei'
+        ])->withLayout(MAIN_LAYOUT));
+        return $response;
+    }
+}
