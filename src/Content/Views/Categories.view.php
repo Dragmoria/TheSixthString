ced3347c3bc0051ac8ac33b3baa1d5ff92a3694d
Iris Jansen <iris.jansen90@outlook.com>
2023-12-20 15:17:46 +0100

    product card omgezet naar een component, aangezien deze op meerdere pagina's voor kan komen

--- src/Content/Views/Categories.view.php
+++ src/Content/Views/Categories.view.php
@@ -62,28 +62,7 @@ foreach ($categories as $category) {
         <div class="row">
             <?php
             foreach ($selectedCategoryProducts as $product) {
-                ?>
-                <div class="col-6 col-md-4 col-lg-2">
-                    <a class="card bg-sixth-beige rounded-4 mb-4 text-decoration-none" href="/Product/<?= $product->id ?>">
-                        <img class="card-img-top thumbnail product-thumbnail rounded-4"
-                             src="<?= $product->media->thumbnail->url ?>"
-                             alt="Category image">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-12">
-                                    <strong><?= $product->name ?></strong>
-                                </div>
-                                <div class="col-12">
-                                    <span>Adviesprijs <?= formatPrice($product->recommendedUnitPrice) ?></span>
-                                </div>
-                                <div class="col-12">
-                                    <strong>Nu <?= formatPrice($product->unitPrice) ?></strong>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <?php
+                echo component(\Http\Controllers\Components\ProductCardComponent::class, (array)$product);
             }
             ?>
         </div>
