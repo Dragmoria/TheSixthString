ca709f4b9d9598f510a5c1dc7f317a583eeb94bf
Jarne Kompier <jko@wigersma-sikkema.com>
2023-12-23 17:32:08 +0100

    Added mail to docker

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,5 +3,7 @@ echo "MYSQL_DATABASE=$MYSQL_DATABASE" >> /var/www/html/.env
 echo "MYSQL_USER=$MYSQL_USER" >> /var/www/html/.env
 echo "MYSQL_SERVER=$MYSQL_SERVERNAME" >> /var/www/html/.env
 echo "MYSQL_PORT=$MYSQL_PORT" >> /var/www/html/.env
+echo "MAIL_PASSWORD_NOREPLY=$MAIL_PASSWORD_NOREPLY" >> /var/www/html/.env
+echo "MAIL_PASSWORD_ADMIN=$MAIL_PASSWORD_ADMIN" >> /var/www/html/.env
 
 exec apache2-foreground
