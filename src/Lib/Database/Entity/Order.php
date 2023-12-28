7cd71e7e354c04b19bb74b0771636857408fc592
icjansen <iris.jansen90@gmail.com>
2023-12-28 15:22:35 +0100

    constraints en kolommen aangepast zodat wanneer een user wordt verwijderd, ook alle adressen worden verwijderd en de address ids in de order-tabel op null worden gezet (#26)
    Co-authored-by: Iris Jansen <iris.jansen90@outlook.com>

--- src/Lib/Database/Entity/Order.php
+++ src/Lib/Database/Entity/Order.php
@@ -7,12 +7,12 @@ class Order extends SaveableObject {
         parent::__construct("`order`"); //order is a keyword, so it's not recognized without the backticks
     }
 
-    public int $userId = 0;
+    public ?int $userId = null;
     public float $orderTotal = 0; //without tax; add $this->>orderTax to show the full order total
     public float $orderTax = 0;
     public ?int $couponId = null;
-    public int $shippingAddressId = 0;
-    public int $invoiceAddressId = 0;
+    public ?int $shippingAddressId = null;
+    public ?int $invoiceAddressId = null;
     public int $paymentStatus = 0; //default is PaymentStatus::AwaitingPayment (= 0)
     public int $shippingStatus = 0; //default is ShippingStatus::AwaitingShipment (= 0)
     public string $createdOn = "";
