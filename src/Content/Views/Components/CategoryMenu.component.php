737dc3ef6250f2ab83c4518dde814e20e63376ef
Jarne Kompier <j.kompier@hotmail.nl>
2023-12-28 16:26:30 +0100

    Header quick fix

--- src/Content/Views/Components/CategoryMenu.component.php
+++ src/Content/Views/Components/CategoryMenu.component.php
@@ -2,29 +2,29 @@
 ?>
 
 <style>
-  .custom-dropdown:hover .dropdown-menu {
-            display: block;
-        }
+    .custom-dropdown:hover .dropdown-menu {
+        display: block;
+    }
 </style>
 
 
-<div class="dropdown custom-dropdown">
-    <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle text-sixth-beige" href="#" id="navbarDropdown" role="button"
-            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+<li class="nav-item dropdown">
+    <div class="dropdown custom-dropdown">
+        <a class="nav-link dropdown-toggle text-sixth-beige" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             Categorieën
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
             <a class="dropdown-item" href="/Category">Alles tonen</a>
             <div class="dropdown-divider"></div>
 
-        <?php
-        foreach($categories as $category) {
-            buildCategoryMenu($category, 0);
+            <?php
+            foreach ($categories as $category) {
+                buildCategoryMenu($category, 0);
             ?>
-            <div class="dropdown-divider"></div>
-        <?php
-        }
-        ?>
+                <div class="dropdown-divider"></div>
+            <?php
+            }
+            ?>
+        </div>
     </div>
 </li>
