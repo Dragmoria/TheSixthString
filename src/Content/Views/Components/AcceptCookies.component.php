4ad69d57282e26a88e01089e66ffb5a1a4003ef8
Jerdi Pol <jerdipol-admin@deltion.nl>
2024-01-04 18:29:25 +0100

    Front page styling etc

--- src/Content/Views/Components/AcceptCookies.component.php
+++ src/Content/Views/Components/AcceptCookies.component.php
@@ -12,22 +12,22 @@
     }
 
     .cookie-modal-content {
-        background-color: #EFE3C4;
+        background-color: var(--sixth-beige);
         margin: 15% auto;
         padding: 20px;
-        border: 1px solid #888;
+        border-radius: 20px;
         width: 80%;
     }
 </style>
 
 <div id="cookieModal" class="cookie-modal">
-    <div class="cookie-modal-content card">
+    <div class="cookie-modal-content card px-5">
         <h2>Cookies op onze website</h2>
         <p>Wij gebruiken cookies om ervoor te zorgen dat we u de beste ervaring op onze website bieden. Als u doorgaat
             zonder uw instellingen te wijzigen, gaan we ervan uit dat u alle cookies op de website ontvangt.</p>
         <div class="col-12 text-center">
             <button class="col-2 rounded-pill"
-                style="background-color:#FCB716;border-color:#FCB716" id="acceptCookies">Accept</button>
+                style="background-color:var(--sixth-yellow); border-color:var(--sixth-yellow)" id="acceptCookies">Accepteren</button>
         </div>
     </div>
 </div>
