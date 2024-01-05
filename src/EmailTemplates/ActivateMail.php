6cd11feaa1065caac09a95b7a4cb4abf3593004b
Jarno Gerrets <jarnogerrets@gmail.com>
2024-01-05 21:31:11 +0100

    fixes 1094235.35

--- src/EmailTemplates/ActivateMail.php
+++ src/EmailTemplates/ActivateMail.php
@@ -3,6 +3,6 @@
 </h1>
 <p>
     Klik op de link om je account te activeren:
-    <a href=http://localhost/Activate/<?= $token ?>>Account activeren</a>
+    <a href="<?="{$_SERVER['REQUEST_SCHEME']}://{$_SERVER['HTTP_HOST']}/Activate/{$token}"?>">Account activeren</a>
     <br><b>Pas na activatie kunt u inloggen.</b>
 </p>
--- src/EmailTemplates/ResetPasswordTemplate.php
+++ src/EmailTemplates/ResetPasswordTemplate.php
@@ -1,5 +1,5 @@
 <h1>Hallo <?=$gebruiker?></h1>
 <p>
     Klik op de link om je wachtwoord te resetten:
-    <a href="http://localhost/ResetPassword/<?= $token ?>">Reset password</a>
+    <a href="<?="{$_SERVER['REQUEST_SCHEME']}://{$_SERVER['HTTP_HOST']}/ResetPassword/{$token}"?>">Reset password</a>
 </p>
