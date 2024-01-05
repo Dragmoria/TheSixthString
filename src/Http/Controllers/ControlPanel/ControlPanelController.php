b5673e79544c84d7a37819cd9c190d870721ffc0
Jarne Kompier <j.kompier@hotmail.nl>
2024-01-05 17:10:52 +0100

    Mail

--- src/Http/Controllers/ControlPanel/ControlPanelController.php
+++ src/Http/Controllers/ControlPanel/ControlPanelController.php
@@ -38,66 +38,10 @@ class ControlPanelController extends Controller
 
         $response = new ViewResponse();
 
-        $currentRole = currentRole();
-
         $response->setBody(view(VIEWS_PATH . 'ControlPanel/ControlPanel.view.php', [
-            "currentRole" => $currentRole->toString()
+            "currentRole" => "piet"
         ])->withLayout(MAIN_LAYOUT));
 
         return $response;
     }
-    /*
-    public function testMail2(): ?Response
-    {
-        $envHandler = Application::getContainer()->resolve(EnvHandler::class);
-
-        $mailTemplate = new MailTemplate(MAIL_TEMPLATES . "ActivateMail.php", ["token" => "123456789"]);
-
-        $mail = new Mail("j.kompier@hotmail.nl", "Test subject", $mailTemplate, MailFrom::NOREPLY, "Mailer name");
-
-        $mail->send();
-
-
-        $domain = $envHandler->getEnv('MAIL_SERVER');
-
-        $apiKey = $envHandler->getEnv('MAIL_API_KEY');
-
-        $encryptKey = $envHandler->getEnv('637f45808aa51ac009b842c4a8d714f8');
-
-        $url = "http://" . $domain . "/SendMail";
-
-        $mailTemplate = new MailTemplate(MAIL_TEMPLATES . "ActivateMail.php", ["token" => "123456789"]);
-
-        $data = [
-            'to' => 'j.kompier@hotmail.nl', // The email address to send to
-            'from' => MailFrom::NOREPLY->value, // The email address to send from
-            'body' => $mailTemplate->render(), // The email body
-            'key' => openssl_encrypt($apiKey, 'AES-128-ECB', $encryptKey) // The encrypted key
-        ];
-
-        $ch = curl_init($url);
-
-        // Set the options for the cURL session
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-        // Execute the cURL session
-        $responseFromMail = curl_exec($ch);
-
-        // Close the cURL session
-        curl_close($ch);
-
-        dump($responseFromMail);
-
-        $response = new ViewResponse();
-
-        $currentRole = currentRole();
-
-        $response->setBody(view(VIEWS_PATH . 'ControlPanel/ControlPanel.view.php', [
-            "currentRole" => $currentRole->toString()
-        ])->withLayout(MAIN_LAYOUT));
-
-        return $response;
-    }*/
 }
